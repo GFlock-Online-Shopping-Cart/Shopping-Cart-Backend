6360e93a849542ce1df1496ae61d0ed1a7b8c0e5
randima.dias <randima.dias@itelasoft.com>
2024-02-26 09:58:09 +0530

    feat: implement remove-cart-item service function

--- src/domain/cartRepository.ts
+++ src/domain/cartRepository.ts
@@ -4,4 +4,5 @@ import { CartItem } from "./entities/cartItem";
 export interface ICartRepository {
     addToCartProduct(productDetails: any): Promise<CartItem>;
     viewCart(cartId: number): Promise<Product[]>
+    removeProductFromCart(productId: number): Promise<CartItem[]>
 }
