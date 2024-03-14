0f38ea72e1fc7c9bbad9fd584e88fd429d76f58f
randima.dias <randima.dias@itelasoft.com>
2024-03-14 08:36:19 +0530

    feat: implement the authentacation middleware

--- src/presentation/routes/cartRoutes.ts
+++ src/presentation/routes/cartRoutes.ts
@@ -1,11 +1,14 @@
 import Container from "typedi";
-import { Router } from "express"; 
+import express, { Router, Request, Response } from "express"; 
 import { CartController } from "../controllers/CartController"; 
+
+import { checkJwt } from "../middleware/auth.middleware";
 
 const router = Router();
 
 const cartController = Container.get(CartController)
 
+router.use(checkJwt);
 router.post("/add-to-cart", cartController.onAddToCart.bind(cartController));
 router.get("/view-cart/:cartId", cartController.onViewCart.bind(cartController));
 router.delete("/remove-product/:cartId/:productId", cartController.onRemoveProductFromCart.bind(cartController));
