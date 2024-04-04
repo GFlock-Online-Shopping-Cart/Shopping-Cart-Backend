8459bf74fe3ad540685fc1dcc501df60d486340e
randima.dias <randima.dias@itelasoft.com>
2024-04-04 16:20:23 +0530

    feat: fix the issues after adding the emailService in test cases

--- src/infrastructure/repositories/checkoutRepository.ts
+++ src/infrastructure/repositories/checkoutRepository.ts
@@ -2,7 +2,6 @@ import { Service } from "typedi";
 import { myDataSource } from "../../config/dataSource";
 import { ICheckoutRepository } from "../../domain/checkoutRepository";
 import { Checkout } from "../../domain/entities/checkout";
-import { CheckoutItem } from "../../domain/entities/checkoutItem";
 
 @Service()
 export class CheckoutRepository implements ICheckoutRepository {
